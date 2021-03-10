a784afc155a1d9546f056c2e6fe1c45c368ddcb0
Sourav Gain <souravgain605@gmail.com>
2021-03-10 19:58:39 +0530

    added more scriptttt

---
+++ compatible.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+check_win_package() {
+    POTH=$1
+    NAME=$2
+    if [[ -d $POTH ]]; then
+        echo -e "\033[1;32mInstalled\t$NAME\033[0m"
+    else
+        echo -e "\033[1;31mNot Installed\t$NAME\033[0m"
+    fi;
+}
+
+check_env_package() {
+    STR=$1
+    NAME=$2
+    if echo "$PATH" | grep -q "$STR"; then
+        echo -e "\033[1;32mInstalled\t$NAME\033[0m"
+    else
+        echo -e "\033[1;31mNot Installed\t$NAME\033[0m"
+    fi
+}
+
+check_unix_package() {
+    PACKAGE=$1
+    NAME=$2
+    dpkg -s $PACKAGE > /dev/null 2>&1
+
+    if [[ $? == 0 ]]; then
+        echo -e "\033[1;32mInstalled\t$NAME\033[0m"
+    else
+        echo -e "\033[1;31mNot Installed\t$NAME\033[0m"
+    fi
+}
+
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    check_win_package "C:/Program Files/MongoDB/Server/" "MongoDB"
+    check_win_package "C:/Program Files/nodejs" "NodeJS"
+    check_env_package "Python" "Python"
+else
+    check_unix_package "nodejs" "NodeJS"
+    check_unix_package "python3" "Python"
+    check_unix_package "mongodb-org" "MongoDB"
+    check_unix_package "samba" "Samba"
+    check_unix_package "samba-common-bin" "Samba common bin"
+fi;
--- setup_master.sh
+++ setup_master.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+getJSON (){
+    echo "\"$1\" : \"$2\","
+}
+getBooleanJSON (){
+    echo "\"$1\" : $2,"
+}
+
 # Remove OLD Server if available
 if [[ -d "master_server" ]]; then
     rm -rf master_server
@@ -29,20 +36,24 @@ KEY=$(openssl rand -base64 32)
 echo "Enter DATABASE NAME: "  
 read DB_NAME
 
+echo "Enter ADMIN KEY: "  
+read ADMIN_KEY
+
 if [[ "$OSTYPE" == "msys" ]]; then
     HOSTNAME="127.0.0.1"
 else
     HOSTNAME=$(hostname -I)
 fi;
 
+# Genarating environment file for master
 cat > .env <<EOF
 STUDENT_CLOUD_KEY=st67hq
 FACULTY_CLOUD_KEY=fu19xa
 DB_URI_STRING=mongodb://localhost:27017/
 DB_NAME=$DB_NAME
-USER_COLLECTION=hello
+USER_COLLECTION=users
 PENDING_USER_COLLECTION=pending_users
-ADMIN_COLLECTION=adminConfig
+ADMIN_COLLECTION=admin_config
 MANAGER_COLLECTION=managers
 SERVER_COLLECTION=servers
 SECRET_KEY=$KEY
@@ -52,9 +63,40 @@ TYPE=dev
 RESTRICT_KEYWORD=raspberry_pi_admin
 EOF
 
+# Database Setup
+echo -e "\
+ use $DB_NAME\n \
+ db.dropDatabase()\n \
+ use $DB_NAME\n \
+ db.createCollection(\"users\")\n \
+ db.createCollection(\"pending_users\")\n \
+ db.createCollection(\"managers\")\n \
+ db.createCollection(\"servers\")\n \
+ db.createCollection(\"admin_config\")\n \
+ db.admin_config.insertOne({\
+ $(getJSON "name" "admin")\
+ $(getJSON "key" "$ADMIN_KEY")\
+ $(getBooleanJSON "addall_inNewHosts" "false")\
+ $(getBooleanJSON "autoStartSrvr" "false")\
+ $(getBooleanJSON "allowRegistration" "true")\
+ $(getBooleanJSON "pendingNewUser" "true")\
+ })" | mongo redirect
+
 # Displaying KEY
 echo ""
+echo "Your SECRET KEY :: "$KEY
+echo "Your ADMIN KEY  :: "$ADMIN_KEY
 echo ""
-echo "Your KEY :: "$KEY
-echo ""
-echo "NOTE - Make sure you copied this key into safe place for other node installations."
+echo "NOTE - Make sure you copied those key into safe place for other node installations."
+
+cd ..
+
+# Saving key into a file
+if [[ -f "secret.keys" ]]; then
+    rm -rf secret.keys
+fi;
+touch secret.keys
+echo -e "\
+Your SECRET KEY :: ${KEY}\n\
+Your ADMIN KEY  :: ${ADMIN_KEY}\n" >> secret.keys
+echo "Your KEY is saved into ${pwd}/secret.keys"

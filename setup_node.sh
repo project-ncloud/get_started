f7876e0f8ca5d45a53dc6e3dc4304a9b2b44d25b
Sourav Gain <souravgain605@gmail.com>
2021-03-10 20:30:03 +0530

    added missing env variable

--- setup_node.sh
+++ setup_node.sh
@@ -29,6 +29,8 @@ deactivate
 # Genarating KEY For master
 echo "Enter Master KEY: "  
 read KEY
+echo "Enter Admin KEY: "  
+read ADMIN_KEY
 
 
 
@@ -44,17 +46,19 @@ else
     CONFIG_FILE="/etc/samba/smb.conf"
 fi;
 
+# Genarating environment file for node
 cat > .env <<EOF
 CONFIG_FILE=$CONFIG_FILE
 SECRET_KEY=$KEY
 OWN_URL=${HOSTNAME}:7000
 PORT=7000
 TYPE=dev
-RESTRICT_KEYWORD=raspberry_pi_admin
+ADMIN_USER=admin
+ADMIN_KEY=$ADMIN_KEY
+RESTRICT_KEYWORD=raspberry_pi_$ADMIN_USER
 EOF
 
 
-echo ""
 echo ""
 echo "DONE..."
 echo ""
